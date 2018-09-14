8c349d2d1f180fbeea1fd720ceb24cc9ea3e6f49
Oliver Gramberg <oliver.gramberg@gmx.de>
2018-09-14 15:56:15 +0200

    Documenting the badge recon problem.

--- webview.js
+++ webview.js
@@ -1,9 +1,17 @@
 module.exports = (Franz) => {
   const getMessages = function getMessages() {
+    
+    // Get count of unread messages; must manually navigate to the "E-Mail" tab.
     let count = 0;
-    //if (document.getElementsByClassName('badge')[0].getAttribute('value') != null) {
-      count = 33; // parseInt(document.getElementsByClassName('badge')[0].innerHTML.replace(/[^0-9.]/g, ''), 10);
-    //}
+    // getElementsByClassName returns an empty array; don't know why: that class 
+    // name is clearly present several times in the DOM. Long line split for debugging.
+    let els = document.getElementsByClassName('badge');
+    count = els.length;
+    let el = els[1];
+    let val = el.getAttribute('value');
+    if (document.getElementsByClassName('badge')[1].getAttribute('value') != null) {
+      count = parseInt(document.getElementsByClassName('badge')[1].innerHTML.replace(/[^0-9.]/g, ''), 10);
+    }
 
     // Just in case we don't end up with a number, set it back to zero (parseInt can return NaN)
     if (isNaN(count)) {
