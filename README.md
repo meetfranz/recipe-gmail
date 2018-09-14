c02995ad6d2db21cc1433897513e51830054cec2
Oliver Gramberg <oliver.gramberg@gmx.de>
2018-09-14 15:08:26 +0200

    Initial version for GMX
    Works except for the new messages indicator.

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# Gmail for Franz
-This is the official Franz recipe for Gmail
+# GMX for Franz
+This is a Franz recipe for GMX. It was contributed by Oliver Gramberg (github:oliver-gramberg).
+
+### Status
+* Works except for the new messages indicator.
 
 ### How to create your own Franz recipes:
 * [Read the documentation](https://github.com/meetfranz/plugins)
--- webview.js
+++ webview.js
@@ -1,29 +1,11 @@
 module.exports = (Franz) => {
   const getMessages = function getMessages() {
     let count = 0;
+    //if (document.getElementsByClassName('badge')[0].getAttribute('value') != null) {
+      count = 33; // parseInt(document.getElementsByClassName('badge')[0].innerHTML.replace(/[^0-9.]/g, ''), 10);
+    //}
 
-    // Each test is done in order of least accurate (but most robust)
-    // -> most accurate (but least robust)
-    // for reliability of at least getting a result
-
-    // 3rd best test (basic, less accurate but OK if nothing else works)
-    if (document.getElementsByClassName('zA zE').length > 0) {
-      count = document.getElementsByClassName('zA zE').length;
-    }
-
-    if (document.getElementsByClassName('J-Ke n0').length > 0) {
-      // 2nd best (more detailed check, much more accurate if available)
-      if (document.getElementsByClassName('J-Ke n0')[0].getAttribute('aria-label') != null) {
-        count = parseInt(document.getElementsByClassName('J-Ke n0')[0].getAttribute('aria-label').replace(/[^0-9.]/g, ''), 10);
-      }
-
-      // 1st best
-      if (document.getElementsByClassName('J-Ke n0')[0].getAttribute('title') != null) {
-        count = parseInt(document.getElementsByClassName('J-Ke n0')[0].getAttribute('title').replace(/[^0-9.]/g, ''), 10);
-      }
-    }
-
-    // Just incase we don't end up with a number, set it back to zero (parseInt can return NaN)
+    // Just in case we don't end up with a number, set it back to zero (parseInt can return NaN)
     if (isNaN(count)) {
       count = 0;
     }
