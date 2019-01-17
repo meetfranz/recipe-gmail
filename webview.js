610095b0ec756f8b9adc78162bfbe0ab3ff79483
Umberto Lentini <umberto.lentini@gmail.com>
2019-01-17 10:34:17 +0100

    Fixed badge count

--- webview.js
+++ webview.js
@@ -1,23 +1,32 @@
-module.exports = (Franz) => {
+module.exports = Franz => {
   const getMessages = function getMessages() {
-    let count = 0;
+    let count = 0
 
-    if (document.getElementsByClassName('J-Ke n0').length > 0) {
-      if (document.getElementsByClassName('J-Ke n0')[0].getAttribute('aria-label') != null) {
-        count = parseInt(document.getElementsByClassName('J-Ke n0')[0].getAttribute('aria-label').replace(/[^0-9.]/g, ''), 10);
+    if (document.getElementsByClassName("J-Ke n0").length > 0) {
+      if (
+        document
+          .getElementsByClassName("J-Ke n0")[0]
+          .getAttribute("aria-label") != null
+      ) {
+        count = parseInt(
+          document
+            .getElementsByClassName("J-Ke n0")[0]
+            .parentElement.parentElement.querySelector(".bsU").innerText,
+          10,
+        )
       }
     }
 
     // Just incase we don't end up with a number, set it back to zero (parseInt can return NaN)
-    count = parseInt(count, 10);
+    count = parseInt(count, 10)
     if (isNaN(count)) {
-      count = 0;
+      count = 0
     }
 
     // set Franz badge
-    Franz.setBadge(count);
-  };
+    Franz.setBadge(count)
+  }
 
   // check for new messages every second and update Franz badge
-  Franz.loop(getMessages);
-};
+  Franz.loop(getMessages)
+}
