893bbe0449b1b5fa6e396a762b116aff439a1b8b
Luca Camillo <luca.camillo@gmail.com>
2021-12-14 09:07:07 +0100

    fix origin header that blocks ajax call between google services

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-// just pass through Franz
-module.exports = Franz => Franz;
+"use strict";
+
+module.exports = Franz => class Gmail extends Franz {
+  modifyRequestHeaders() {
+                        return [{
+                                headers: {
+                                        'origin': 'RefererHost'
+                                },
+                                requestFilters: {
+                                        urls: [
+                                        '*://people-pa.clients6.google.com/*', '*://clients6.google.com/drive/*', '*://clients6.google.com/gmail/*', '*://signaler-pa.clients6.google.com/*', '*://addons-pa.clients6.google.com/*',
+                                        '*://chat-pa.clients6.google.com/*','*://clients6.google.com/voice/*', '*://hangouts.google.com/*',
+                                        '*://peoplestack-pa.clients6.google.com/*', '*://calendar-pa.clients6.google.com/*', '*://play.google.com/*']
+                                }
+                        }]
+  }
+
+};
