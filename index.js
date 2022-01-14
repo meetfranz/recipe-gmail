3310f29066b34c9918aac0e57d3032ae54a21eee
Luca Camillo <luca.camillo@gmail.com>
2022-01-14 16:07:04 +0100

    add new host

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = Franz => class Gmail extends Franz {
         origin: 'RefererHost'
       },
       requestFilters: {
-        urls: ['*://people-pa.clients6.google.com/*', '*://peoplestackwebexperiments-pa.clients6.google.com/*', '*://clients6.google.com/drive/*', '*://clients6.google.com/gmail/*', '*://signaler-pa.clients6.google.com/*', '*://addons-pa.clients6.google.com/*', '*://chat-pa.clients6.google.com/*', '*://clients6.google.com/voice/*', '*://hangouts.google.com/*', '*://peoplestack-pa.clients6.google.com/*', '*://calendar-pa.clients6.google.com/*', '*://play.google.com/*']
+        urls: ['*://taskassist-pa.clients6.google.com/*', '*://people-pa.clients6.google.com/*', '*://peoplestackwebexperiments-pa.clients6.google.com/*', '*://clients6.google.com/drive/*', '*://clients6.google.com/gmail/*', '*://signaler-pa.clients6.google.com/*', '*://addons-pa.clients6.google.com/*', '*://chat-pa.clients6.google.com/*', '*://clients6.google.com/voice/*', '*://hangouts.google.com/*', '*://peoplestack-pa.clients6.google.com/*', '*://calendar-pa.clients6.google.com/*', '*://play.google.com/*']
       }
     }];
   }
